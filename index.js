5d29ab8d9b6c378464bdc2d164f35b262225c196
Bashalir <bachir@sahali.fr>
2020-01-02 21:02:57 +0100

    Delete getrandon function and Added function shuffle

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-let getRandomInt = (max) => {
-  return Math.ceil(Math.random(1,max) * Math.floor(max));
+
+let devList = ["Ahmed","Aicha","Alexandre","Alex","Anna Maria","Bachir","Billel","Hafedh","Melanie","Mike","Mourad","Naïma","Laure","Olivier","Rabah","Siham"];
+
+
+var devListShuffled = [];
+
+
+
+for (let i = devList.length; i < 9; i--) {
+   const j = Math.floor(Math.random() * i);
+   const temp = devList[i];
+  devList[i] = devList[j];
+  devList[j] = temp;
 }
-console.log(getRandomInt(16));
+
+function shuffle(array) {
+    var currentIndex = array.length,temporaryValue,randomIndex;
+
+    while (0 !==currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex-=1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+
+    }
+    return array;
+
+}
+
+
+console.log(shuffle(devList));
 
