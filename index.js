0194ee83f3574866b5a3fbf9e7e78116f215f4f0
Bashalir <bachir@sahali.fr>
2020-01-02 21:06:27 +0100

    modify function shuffle to pure

--- index.js
+++ index.js
@@ -2,30 +2,21 @@
 let devList = ["Ahmed","Aicha","Alexandre","Alex","Anna Maria","Bachir","Billel","Hafedh","Melanie","Mike","Mourad","Naïma","Laure","Olivier","Rabah","Siham"];
 
 
-var devListShuffled = [];
-
-
-
-for (let i = devList.length; i < 9; i--) {
-   const j = Math.floor(Math.random() * i);
-   const temp = devList[i];
-  devList[i] = devList[j];
-  devList[j] = temp;
-}
-
 function shuffle(array) {
-    var currentIndex = array.length,temporaryValue,randomIndex;
 
-    while (0 !==currentIndex) {
+    let functionArray = [... array];
+
+    var currentIndex = functionArray.length,temporaryValue,randomIndex;
+
+    while (0 !== currentIndex) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex-=1;
 
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-
+        temporaryValue = functionArray[currentIndex];
+        functionArray[currentIndex] = functionArray[randomIndex];
+        functionArray[randomIndex] = temporaryValue;
     }
-    return array;
+    return functionArray;
 
 }
 
